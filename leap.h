cc0dc58fa74ad345ba7cadabbbc4eac46c6a0603
Paul51 <nilotpaul51@gmail.com>
2016-09-02 20:16:07 +0530

    header for leap test

---
+++ leap.h
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class is_leap_year
+{
+	public:
+	int year;
+	is_leap_year()
+	{
+		if(year%4==0||year%100==0)
+		{
+		    cout<<"Is a leap year"<<endl;
+                }
+		else if(year%400==0)
+       		{
+			cout<<"is a leap year"<<endl;
+		  
+		}
+	        else
+		{
+		        cout<<"Is not a leap  year"<<endl;
+		}
+
+	}
+};
