ae97f50f4f2a08486b65b68bac27c1e4169a7dd4
opu12 <deepopu09@gmail.com>
2016-09-07 11:27:39 +0530

    Update selftest1.c

--- selftest1.c
+++ selftest1.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-  int lowrange,upprange,evensum,oddsum,i;
+  int lowrange,upprange,evensum=0,oddsum=0,i;
   printf("enter the lower limit of the range \n");
   scanf("%d",&lowrange);
   printf("enter the upper limit of the range \n");
@@ -11,14 +11,13 @@ int main()
     if(i%2==0)
     {
     	evensum += i;
-    	printf("the sum of all even numbers is %d \n",evensum);
     }
     else
     {
       	oddsum += i;
-      	printf("the sum of all odd numbers is %d \n",oddsum);
-    }
   }
-
-};
+  }
+printf("the sum of all even numbers is %d \n",evensum);
+	printf("the sum of all odd numbers is %d \n",oddsum);
+}
 
